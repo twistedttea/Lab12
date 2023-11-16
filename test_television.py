a339374cbc648aac2b8b310c5dd5c38699be4917
twistedttea <twistedtteavr@gmail.com>
2023-11-16 11:51:51 -0600

    test updated

---
+++ test_television.py
@@ -0,0 +1,11 @@
+import pytest 
+from television import Television
+class Test:
+    def setup_method(self):
+        self.tv0 = Television()
+        self.tv1 = Television()
+
+    def test_construction(self):
+        assert self.tv0.__str__() == 'Power - False, Channel - 0, Volume - 0'
+        assert self.tv1.__str__() == 'Power - False, Channel - 0, Volume - 0'
+
