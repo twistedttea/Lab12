a38baabeda1e1373ba8fcefec2ceb43cea40787c
twistedttea <twistedtteavr@gmail.com>
2023-11-16 22:29:25 -0600

    unit tests complete

--- test_television.py
+++ test_television.py
@@ -1,5 +1,5 @@
-from television import Television 
-
+import pytest 
+from television import Television
 
 
 def test_tv_init():
@@ -40,9 +40,78 @@ def test_channel_up():
     tv.power()
     tv.channel_up()
     assert str(tv) == 'Power = True, Channel = 1, Volume = 0'
+    tv.power()
+    tv.channel_up()
+    assert str(tv) == 'Power = False, Channel = 1, Volume = 0'
+    tv.power()
 
     
-    for _ in range(10):
+    for _ in range(4):
         tv.channel_up()
 
+    assert str(tv) == 'Power = True, Channel = 1, Volume = 0'
+
+def test_channel_down():
+    tv = Television()
+
+    tv.power()
+    tv.channel_down()
     assert str(tv) == 'Power = True, Channel = 3, Volume = 0'
+    tv.power()
+    tv.channel_up()
+    assert str(tv) == 'Power = False, Channel = 3, Volume = 0'
+    tv.power()
+
+    for _ in range(4):
+        tv.channel_down()
+    assert str(tv) == 'Power = True, Channel = 3, Volume = 0'
+
+def test_volume_up():
+    tv = Television()
+
+    tv.power()
+    tv.volume_up()
+    assert str(tv) == 'Power = True, Channel = 0, Volume = 1'
+    
+    tv.power()
+    tv.volume_up()
+    assert str(tv) == 'Power = False, Channel = 0, Volume = 1'
+    
+    tv.power()
+    tv.mute()
+    tv.volume_up()
+    assert str(tv) == 'Power = True, Channel = 0, Volume = 2'
+    
+    for _ in range(6):
+        tv.volume_up()
+    assert str(tv) == 'Power = True, Channel = 0, Volume = 2'
+
+
+def test_volume_down():
+    tv = Television()
+
+    tv.power()
+    tv.volume_up()
+    tv.volume_up()
+    tv.volume_down()
+    assert str(tv) == 'Power = True, Channel = 0, Volume = 1'
+    
+    tv.volume_up()
+    tv.power()
+    tv.volume_down()
+    assert str(tv) == 'Power = False, Channel = 0, Volume = 2'
+    
+    tv.power()
+    tv.volume_up()
+    tv.mute()
+    tv.volume_down()
+    assert str(tv) == 'Power = True, Channel = 0, Volume = 1'
+    
+    for _ in range(6):
+        tv.volume_down()
+    assert str(tv) == 'Power = True, Channel = 0, Volume = 0'
+
+
+    
+
+ 
